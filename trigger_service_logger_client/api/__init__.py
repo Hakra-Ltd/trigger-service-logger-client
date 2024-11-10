08331a85058465a2a61ad628c7d773ce40293c5a
Jiri Sevcik <jiri.sevcik@primehammer.com>
2024-11-10 19:12:14 +0100

    Update client

---
+++ trigger_service_logger_client/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from trigger_service_logger_client.api.job_api import JobApi
+from trigger_service_logger_client.api.default_api import DefaultApi
+
