32f7a3b8ba4bcaa819e5f961eb6d8479c8bb2989
Jiri Sevcik <jiri.sevcik@primehammer.com>
2024-12-18 14:04:22 +0100

    Update inputs

--- trigger_service_logger_client/__init__.py
+++ trigger_service_logger_client/__init__.py
@@ -17,6 +17,7 @@
 __version__ = "1.0.0"
 
 # import apis into sdk package
+from trigger_service_logger_client.api.event_api import EventApi
 from trigger_service_logger_client.api.job_api import JobApi
 from trigger_service_logger_client.api.default_api import DefaultApi
 
--- trigger_service_logger_client/api/__init__.py
+++ trigger_service_logger_client/api/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 
 # import apis into api package
+from trigger_service_logger_client.api.event_api import EventApi
 from trigger_service_logger_client.api.job_api import JobApi
 from trigger_service_logger_client.api.default_api import DefaultApi
 
